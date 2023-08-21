440922caf2e7447b44db0ef210bbbf10306621b9
ArtemSuhalet <123483087+ArtemSuhalet@users.noreply.github.com>
2023-08-21 11:18:08 +0300

    Add files via upload

---
+++ database/record.py
@@ -0,0 +1,31 @@
+from database.meet_process import *
+import soundcard as sc
+import soundfile as sf
+
+
+
+
+
+
+def record_audio(recording_flag):
+    OUTPUT_FILE_NAME = "out.wav"  # Имя файла.
+    SAMPLE_RATE = 48000  # [Гц]. Частота дискретизации.
+
+    with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
+        while recording_flag:
+            data = mic.record(numframes=SAMPLE_RATE)
+            sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+
+# def record_audio():
+#     global recording_flag
+#     num_frames_to_record = int(20 * SAMPLE_RATE)  # Записать 20 секунд аудио
+#
+#     with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
+#         data = mic.record(numframes=num_frames_to_record)
+#         sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+
+# def stop_recording():
+#     global recording_flag
+#     recording_flag = False
+
+
