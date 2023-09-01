eaa092df49162fd3aba1eadf7c1cf8694918ac11
ArtemSuhalet <123483087+ArtemSuhalet@users.noreply.github.com>
2023-09-01 15:16:39 +0300

    Update data.py

--- database/data.py
+++ database/data.py
@@ -8,46 +8,6 @@ import textwrap
 openai.api_key = os.getenv('KEY')
 
 
-# class BaseModel(Model):
-#     class Meta:
-#         database = my_db
-#         db_table = 'table'
-#
-#
-# class User_Data(BaseModel):
-#     user_telegram_id = IntegerField()
-#
-#
-# def create_db() -> None:
-#     """
-#     Функция создает базу данных, если она отсутствует.
-#     :return:
-#     """
-#     try:
-#         my_db.connect()
-#         User_Data.create_table()
-#     except InternalError as px:
-#         print(str(px))
-#
-#
-# def add_user_data(user_telegram_id) -> None:
-#     """
-#     Функция создает запись в базе данных.
-#     :param user_telegram_id:
-#     :param command:
-#     :param request_time:
-#     :param text_for_database:
-#     :return:
-#     """
-#
-#     with my_db:
-#         User_Data.create(user_telegram_id=user_telegram_id,
-#                             user_command=command,
-#                             user_time_request=request_time,
-#                             user_hotels_list=text_for_database
-#                             )
-
-
 def read_file_request():
     """
     Функция для чтения запроса из файла
